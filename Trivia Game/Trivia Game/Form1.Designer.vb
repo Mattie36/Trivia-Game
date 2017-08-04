f3537147ab2e768df7f8fb5fda5a27ec103e5a5d
CCLC <cclc@lr.k12.nj.us>
2017-08-04 10:25:23 -0400

    Trivia Game

---
+++ Trivia Game/Trivia Game/Form1.Designer.vb
@@ -0,0 +1,452 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Q1 = New System.Windows.Forms.Button()
+        Me.Q2 = New System.Windows.Forms.Button()
+        Me.Q3 = New System.Windows.Forms.Button()
+        Me.Q4 = New System.Windows.Forms.Button()
+        Me.Q5 = New System.Windows.Forms.Button()
+        Me.Q6 = New System.Windows.Forms.Button()
+        Me.Q7 = New System.Windows.Forms.Button()
+        Me.Q8 = New System.Windows.Forms.Button()
+        Me.Q9 = New System.Windows.Forms.Button()
+        Me.Q10 = New System.Windows.Forms.Button()
+        Me.Q11 = New System.Windows.Forms.Button()
+        Me.Q12 = New System.Windows.Forms.Button()
+        Me.Q13 = New System.Windows.Forms.Button()
+        Me.Q14 = New System.Windows.Forms.Button()
+        Me.Q15 = New System.Windows.Forms.Button()
+        Me.Q16 = New System.Windows.Forms.Button()
+        Me.Q17 = New System.Windows.Forms.Button()
+        Me.Q18 = New System.Windows.Forms.Button()
+        Me.Q19 = New System.Windows.Forms.Button()
+        Me.Q20 = New System.Windows.Forms.Button()
+        Me.Question = New System.Windows.Forms.Label()
+        Me.Answer1 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Submit = New System.Windows.Forms.Button()
+        Me.Answer4 = New System.Windows.Forms.RadioButton()
+        Me.Answer3 = New System.Windows.Forms.RadioButton()
+        Me.Answer2 = New System.Windows.Forms.RadioButton()
+        Me.Score_l = New System.Windows.Forms.Label()
+        Me.QuestionsAnswered = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Q1
+        '
+        Me.Q1.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q1.Location = New System.Drawing.Point(11, 14)
+        Me.Q1.Name = "Q1"
+        Me.Q1.Size = New System.Drawing.Size(107, 32)
+        Me.Q1.TabIndex = 0
+        Me.Q1.Text = "Question 1"
+        Me.Q1.UseVisualStyleBackColor = True
+        '
+        'Q2
+        '
+        Me.Q2.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q2.Location = New System.Drawing.Point(124, 14)
+        Me.Q2.Name = "Q2"
+        Me.Q2.Size = New System.Drawing.Size(107, 32)
+        Me.Q2.TabIndex = 1
+        Me.Q2.Text = "Question 2"
+        Me.Q2.UseVisualStyleBackColor = True
+        '
+        'Q3
+        '
+        Me.Q3.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q3.Location = New System.Drawing.Point(237, 14)
+        Me.Q3.Name = "Q3"
+        Me.Q3.Size = New System.Drawing.Size(107, 32)
+        Me.Q3.TabIndex = 2
+        Me.Q3.Text = "Question 3"
+        Me.Q3.UseVisualStyleBackColor = True
+        '
+        'Q4
+        '
+        Me.Q4.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q4.Location = New System.Drawing.Point(350, 14)
+        Me.Q4.Name = "Q4"
+        Me.Q4.Size = New System.Drawing.Size(107, 32)
+        Me.Q4.TabIndex = 3
+        Me.Q4.Text = "Question 4"
+        Me.Q4.UseVisualStyleBackColor = True
+        '
+        'Q5
+        '
+        Me.Q5.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q5.Location = New System.Drawing.Point(463, 14)
+        Me.Q5.Name = "Q5"
+        Me.Q5.Size = New System.Drawing.Size(107, 32)
+        Me.Q5.TabIndex = 4
+        Me.Q5.Text = "Question 5"
+        Me.Q5.UseVisualStyleBackColor = True
+        '
+        'Q6
+        '
+        Me.Q6.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q6.Location = New System.Drawing.Point(576, 14)
+        Me.Q6.Name = "Q6"
+        Me.Q6.Size = New System.Drawing.Size(107, 32)
+        Me.Q6.TabIndex = 5
+        Me.Q6.Text = "Question 6"
+        Me.Q6.UseVisualStyleBackColor = True
+        '
+        'Q7
+        '
+        Me.Q7.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q7.Location = New System.Drawing.Point(689, 14)
+        Me.Q7.Name = "Q7"
+        Me.Q7.Size = New System.Drawing.Size(107, 32)
+        Me.Q7.TabIndex = 6
+        Me.Q7.Text = "Question 7"
+        Me.Q7.UseVisualStyleBackColor = True
+        '
+        'Q8
+        '
+        Me.Q8.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q8.Location = New System.Drawing.Point(802, 14)
+        Me.Q8.Name = "Q8"
+        Me.Q8.Size = New System.Drawing.Size(107, 32)
+        Me.Q8.TabIndex = 7
+        Me.Q8.Text = "Question 8"
+        Me.Q8.UseVisualStyleBackColor = True
+        '
+        'Q9
+        '
+        Me.Q9.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q9.Location = New System.Drawing.Point(915, 14)
+        Me.Q9.Name = "Q9"
+        Me.Q9.Size = New System.Drawing.Size(107, 32)
+        Me.Q9.TabIndex = 8
+        Me.Q9.Text = "Question 9"
+        Me.Q9.UseVisualStyleBackColor = True
+        '
+        'Q10
+        '
+        Me.Q10.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q10.Location = New System.Drawing.Point(1027, 14)
+        Me.Q10.Name = "Q10"
+        Me.Q10.Size = New System.Drawing.Size(107, 32)
+        Me.Q10.TabIndex = 9
+        Me.Q10.Text = "Question 10"
+        Me.Q10.UseVisualStyleBackColor = True
+        '
+        'Q11
+        '
+        Me.Q11.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q11.Location = New System.Drawing.Point(11, 29)
+        Me.Q11.Name = "Q11"
+        Me.Q11.Size = New System.Drawing.Size(107, 32)
+        Me.Q11.TabIndex = 10
+        Me.Q11.Text = "Question 11"
+        Me.Q11.UseVisualStyleBackColor = True
+        '
+        'Q12
+        '
+        Me.Q12.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q12.Location = New System.Drawing.Point(124, 29)
+        Me.Q12.Name = "Q12"
+        Me.Q12.Size = New System.Drawing.Size(107, 32)
+        Me.Q12.TabIndex = 11
+        Me.Q12.Text = "Question 12"
+        Me.Q12.UseVisualStyleBackColor = True
+        '
+        'Q13
+        '
+        Me.Q13.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q13.Location = New System.Drawing.Point(237, 29)
+        Me.Q13.Name = "Q13"
+        Me.Q13.Size = New System.Drawing.Size(107, 32)
+        Me.Q13.TabIndex = 12
+        Me.Q13.Text = "Question 13"
+        Me.Q13.UseVisualStyleBackColor = True
+        '
+        'Q14
+        '
+        Me.Q14.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q14.Location = New System.Drawing.Point(350, 29)
+        Me.Q14.Name = "Q14"
+        Me.Q14.Size = New System.Drawing.Size(107, 32)
+        Me.Q14.TabIndex = 13
+        Me.Q14.Text = "Question 14"
+        Me.Q14.UseVisualStyleBackColor = True
+        '
+        'Q15
+        '
+        Me.Q15.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q15.Location = New System.Drawing.Point(463, 29)
+        Me.Q15.Name = "Q15"
+        Me.Q15.Size = New System.Drawing.Size(107, 32)
+        Me.Q15.TabIndex = 14
+        Me.Q15.Text = "Question 15"
+        Me.Q15.UseVisualStyleBackColor = True
+        '
+        'Q16
+        '
+        Me.Q16.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q16.Location = New System.Drawing.Point(576, 29)
+        Me.Q16.Name = "Q16"
+        Me.Q16.Size = New System.Drawing.Size(107, 32)
+        Me.Q16.TabIndex = 15
+        Me.Q16.Text = "Question 16"
+        Me.Q16.UseVisualStyleBackColor = True
+        '
+        'Q17
+        '
+        Me.Q17.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q17.Location = New System.Drawing.Point(689, 29)
+        Me.Q17.Name = "Q17"
+        Me.Q17.Size = New System.Drawing.Size(107, 32)
+        Me.Q17.TabIndex = 16
+        Me.Q17.Text = "Question 17"
+        Me.Q17.UseVisualStyleBackColor = True
+        '
+        'Q18
+        '
+        Me.Q18.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q18.Location = New System.Drawing.Point(802, 29)
+        Me.Q18.Name = "Q18"
+        Me.Q18.Size = New System.Drawing.Size(107, 32)
+        Me.Q18.TabIndex = 17
+        Me.Q18.Text = "Question 18"
+        Me.Q18.UseVisualStyleBackColor = True
+        '
+        'Q19
+        '
+        Me.Q19.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q19.Location = New System.Drawing.Point(915, 29)
+        Me.Q19.Name = "Q19"
+        Me.Q19.Size = New System.Drawing.Size(107, 32)
+        Me.Q19.TabIndex = 18
+        Me.Q19.Text = "Question 19"
+        Me.Q19.UseVisualStyleBackColor = True
+        '
+        'Q20
+        '
+        Me.Q20.Font = New System.Drawing.Font("Mistral", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q20.Location = New System.Drawing.Point(1027, 29)
+        Me.Q20.Name = "Q20"
+        Me.Q20.Size = New System.Drawing.Size(107, 32)
+        Me.Q20.TabIndex = 19
+        Me.Q20.Text = "Question 20"
+        Me.Q20.UseVisualStyleBackColor = True
+        '
+        'Question
+        '
+        Me.Question.AutoSize = True
+        Me.Question.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Question.Location = New System.Drawing.Point(346, 105)
+        Me.Question.Name = "Question"
+        Me.Question.Size = New System.Drawing.Size(168, 26)
+        Me.Question.TabIndex = 20
+        Me.Question.Text = "Select a Question"
+        '
+        'Answer1
+        '
+        Me.Answer1.AutoSize = True
+        Me.Answer1.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Answer1.Location = New System.Drawing.Point(3, 3)
+        Me.Answer1.Name = "Answer1"
+        Me.Answer1.Size = New System.Drawing.Size(131, 21)
+        Me.Answer1.TabIndex = 21
+        Me.Answer1.TabStop = True
+        Me.Answer1.Text = "Select a Question"
+        Me.Answer1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Submit)
+        Me.Panel1.Controls.Add(Me.Answer4)
+        Me.Panel1.Controls.Add(Me.Answer3)
+        Me.Panel1.Controls.Add(Me.Answer2)
+        Me.Panel1.Controls.Add(Me.Answer1)
+        Me.Panel1.Location = New System.Drawing.Point(510, 167)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(303, 176)
+        Me.Panel1.TabIndex = 22
+        '
+        'Submit
+        '
+        Me.Submit.Font = New System.Drawing.Font("Modern No. 20", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Submit.Location = New System.Drawing.Point(3, 130)
+        Me.Submit.Name = "Submit"
+        Me.Submit.Size = New System.Drawing.Size(124, 32)
+        Me.Submit.TabIndex = 25
+        Me.Submit.Text = "Submit"
+        Me.Submit.UseVisualStyleBackColor = True
+        '
+        'Answer4
+        '
+        Me.Answer4.AutoSize = True
+        Me.Answer4.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Answer4.Location = New System.Drawing.Point(3, 84)
+        Me.Answer4.Name = "Answer4"
+        Me.Answer4.Size = New System.Drawing.Size(131, 21)
+        Me.Answer4.TabIndex = 23
+        Me.Answer4.TabStop = True
+        Me.Answer4.Text = "Select a Question"
+        Me.Answer4.UseVisualStyleBackColor = True
+        '
+        'Answer3
+        '
+        Me.Answer3.AutoSize = True
+        Me.Answer3.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Answer3.Location = New System.Drawing.Point(3, 57)
+        Me.Answer3.Name = "Answer3"
+        Me.Answer3.Size = New System.Drawing.Size(131, 21)
+        Me.Answer3.TabIndex = 23
+        Me.Answer3.TabStop = True
+        Me.Answer3.Text = "Select a Question"
+        Me.Answer3.UseVisualStyleBackColor = True
+        '
+        'Answer2
+        '
+        Me.Answer2.AutoSize = True
+        Me.Answer2.Font = New System.Drawing.Font("Modern No. 20", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Answer2.Location = New System.Drawing.Point(3, 30)
+        Me.Answer2.Name = "Answer2"
+        Me.Answer2.Size = New System.Drawing.Size(131, 21)
+        Me.Answer2.TabIndex = 22
+        Me.Answer2.TabStop = True
+        Me.Answer2.Text = "Select a Question"
+        Me.Answer2.UseVisualStyleBackColor = True
+        '
+        'Score_l
+        '
+        Me.Score_l.AutoSize = True
+        Me.Score_l.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Score_l.Location = New System.Drawing.Point(12, 71)
+        Me.Score_l.Name = "Score_l"
+        Me.Score_l.Size = New System.Drawing.Size(70, 26)
+        Me.Score_l.TabIndex = 23
+        Me.Score_l.Text = "Score:"
+        '
+        'QuestionsAnswered
+        '
+        Me.QuestionsAnswered.AutoSize = True
+        Me.QuestionsAnswered.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.QuestionsAnswered.Location = New System.Drawing.Point(882, 71)
+        Me.QuestionsAnswered.Name = "QuestionsAnswered"
+        Me.QuestionsAnswered.Size = New System.Drawing.Size(191, 26)
+        Me.QuestionsAnswered.TabIndex = 24
+        Me.QuestionsAnswered.Text = "Questions Answered"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Q1)
+        Me.Panel2.Controls.Add(Me.Q2)
+        Me.Panel2.Controls.Add(Me.Q3)
+        Me.Panel2.Controls.Add(Me.Q4)
+        Me.Panel2.Controls.Add(Me.Q5)
+        Me.Panel2.Controls.Add(Me.Q6)
+        Me.Panel2.Controls.Add(Me.Q7)
+        Me.Panel2.Controls.Add(Me.Q8)
+        Me.Panel2.Controls.Add(Me.Q9)
+        Me.Panel2.Controls.Add(Me.Q10)
+        Me.Panel2.Location = New System.Drawing.Point(1, -2)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1137, 58)
+        Me.Panel2.TabIndex = 25
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Q11)
+        Me.Panel3.Controls.Add(Me.Q12)
+        Me.Panel3.Controls.Add(Me.Q13)
+        Me.Panel3.Controls.Add(Me.Q14)
+        Me.Panel3.Controls.Add(Me.Q15)
+        Me.Panel3.Controls.Add(Me.Q17)
+        Me.Panel3.Controls.Add(Me.Q20)
+        Me.Panel3.Controls.Add(Me.Q16)
+        Me.Panel3.Controls.Add(Me.Q19)
+        Me.Panel3.Controls.Add(Me.Q18)
+        Me.Panel3.Location = New System.Drawing.Point(1, 409)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1137, 74)
+        Me.Panel3.TabIndex = 26
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1139, 482)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.QuestionsAnswered)
+        Me.Controls.Add(Me.Score_l)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Question)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1155, 521)
+        Me.MinimumSize = New System.Drawing.Size(1155, 521)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Q1 As Button
+    Friend WithEvents Q2 As Button
+    Friend WithEvents Q3 As Button
+    Friend WithEvents Q4 As Button
+    Friend WithEvents Q5 As Button
+    Friend WithEvents Q6 As Button
+    Friend WithEvents Q7 As Button
+    Friend WithEvents Q8 As Button
+    Friend WithEvents Q9 As Button
+    Friend WithEvents Q10 As Button
+    Friend WithEvents Q11 As Button
+    Friend WithEvents Q12 As Button
+    Friend WithEvents Q13 As Button
+    Friend WithEvents Q14 As Button
+    Friend WithEvents Q15 As Button
+    Friend WithEvents Q16 As Button
+    Friend WithEvents Q17 As Button
+    Friend WithEvents Q18 As Button
+    Friend WithEvents Q19 As Button
+    Friend WithEvents Q20 As Button
+    Friend WithEvents Question As Label
+    Friend WithEvents Answer1 As RadioButton
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Answer4 As RadioButton
+    Friend WithEvents Answer3 As RadioButton
+    Friend WithEvents Answer2 As RadioButton
+    Friend WithEvents Score_l As Label
+    Friend WithEvents QuestionsAnswered As Label
+    Friend WithEvents Submit As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+End Class
---
+++ Trivia Game/Trivia Game/Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+    Dim correct As Integer
+    Dim score As Integer = 0
+    Dim answered As Integer = 0
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Q1.Click
+        Q1.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "How many teams are in the National Football League?"
+        Answer1.Text = "20"
+        Answer2.Text = "32"
+        Answer3.Text = "24"
+        Answer4.Text = "30"
+        correct = 2
+
+    End Sub
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        Dim userAnswer As Integer
+
+        If Answer1.Checked Then
+            userAnswer = 1
+        ElseIf Answer2.Checked Then
+            userAnswer = 2
+        ElseIf Answer3.Checked Then
+            userAnswer = 3
+        ElseIf Answer4.Checked Then
+            userAnswer = 4
+        End If
+
+        If userAnswer = correct Then
+            MsgBox("Correct!")
+            score = score + 1
+            answered = answered + 1
+        Else
+            MsgBox("Incorrect")
+            answered = answered + 1
+        End If
+
+        Score_l.Text = "Score: " & score
+        QuestionsAnswered.Text = "Questions Answered: " & answered & "/20"
+
+        Panel1.Enabled = False
+        Panel2.Enabled = True
+        Panel3.Enabled = True
+
+
+    End Sub
+
+    Private Sub Q2_Click(sender As Object, e As EventArgs) Handles Q2.Click
+        Q2.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What team has the most SuperBowl wins?"
+        Answer1.Text = "New England Patriots"
+        Answer2.Text = "Seattle Seahawks"
+        Answer3.Text = "Pittsburgh Steelers"
+        Answer4.Text = "Denver Broncos"
+        correct = 3
+    End Sub
+
+    Private Sub Q3_Click(sender As Object, e As EventArgs) Handles Q3.Click
+        Q3.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "Who is the starting quarterback for the Miami Dolphins?"
+        Answer1.Text = "Ryan Tannehill"
+        Answer2.Text = "Ben Rothlisburger"
+        Answer3.Text = "Cam Newton"
+        Answer4.Text = "Matt Moore"
+        correct = 1
+    End Sub
+
+    Private Sub Q4_Click(sender As Object, e As EventArgs) Handles Q4.Click
+        Q4.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What teams were in Super Bowl 50?"
+        Answer1.Text = "New England Patriots and Seattle Seahawks"
+        Answer2.Text = "New England Patriots and Atlanta Falcons"
+        Answer3.Text = "Seattle Seahawks and Denver Broncos"
+        Answer4.Text = "Denver Broncos and Carolina Panthers"
+        correct = 4
+    End Sub
+
+    Private Sub Q5_Click(sender As Object, e As EventArgs) Handles Q5.Click
+        Q5.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "How many teams in the NFL have never won a Super Bowl?"
+        Answer1.Text = "5"
+        Answer2.Text = "15"
+        Answer3.Text = "0"
+        Answer4.Text = "13"
+        correct = 4
+    End Sub
+
+    Private Sub Q6_Click(sender As Object, e As EventArgs) Handles Q6.Click
+        Q6.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What team's fans are called Cheeseheads?"
+        Answer1.Text = "Tampa Bay Buccaneers"
+        Answer2.Text = "Green Bay Packers"
+        Answer3.Text = "San Diego Chargers"
+        Answer4.Text = "Oakland Raiders"
+        correct = 2
+    End Sub
+
+    Private Sub Q7_Click(sender As Object, e As EventArgs) Handles Q7.Click
+        Q7.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What team plays at Bank of America Stadium?"
+        Answer1.Text = "Carolina Panthers"
+        Answer2.Text = "New England Patriots"
+        Answer3.Text = "Philadelphia Eagles"
+        Answer4.Text = "Cleveland Browns"
+        correct = 1
+    End Sub
+
+    Private Sub Q8_Click(sender As Object, e As EventArgs) Handles Q8.Click
+        Q8.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What is the New England Patriots most recent scandal?"
+        Answer1.Text = "Spy-Gate"
+        Answer2.Text = "Snowplow-Gate"
+        Answer3.Text = "Deflate-Gate"
+        Answer4.Text = "Deception-Gate"
+        correct = 3
+    End Sub
+
+    Private Sub Q9_Click(sender As Object, e As EventArgs) Handles Q9.Click
+        Q9.Enabled = False
+        Panel1.Enabled = True
+        Panel2.Enabled = False
+        Panel3.Enabled = False
+        Question.Text = "What year was the first Super Bowl played?"
+        Answer1.
+    End Sub
+End Class
